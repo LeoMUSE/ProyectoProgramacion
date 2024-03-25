724ccfce79492e2b666cd426f5db5d3b76ec6021
VicenteCaraT <vit.cara@alumno.um.edu.ar>
2024-03-25 08:08:24 -0300

    merging branches

--- backend/main/resources/__init__.py
+++ backend/main/resources/__init__.py
@@ -1,5 +1,8 @@
 from .login import Login as LoginResource
+from .signIn import SignIn as SignInResource
 from .usuario import Usuario as UsuarioResource
 from .usuario import Usuarios as UsuariosResource
 from .libro import Libro as LibroResource
 from .libro import Libros as LibrosResourse
+from .prestamo import Prestamo as PrestamoResourse
+from .prestamo import Prestamos as PrestamosResourse
