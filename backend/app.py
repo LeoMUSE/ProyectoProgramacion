0d4d9027dc769f06c00300f42db861d42f093031
diego <diego@diegodeMacBook-Air.local>
2024-04-09 17:09:36 -0300

    d

--- backend/app.py
+++ backend/app.py
@@ -7,7 +7,11 @@ app = create_app()
 app.app_context().push()
 
 
+from main import db
 if __name__ == '__main__':
+    # app.run(debug=True,port=os.getenv('PORT'))
+    db.create_all()
     app.run(debug=True,port=os.getenv('PORT'))
+    
 
 
