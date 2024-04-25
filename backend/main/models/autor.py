51f2352335b5f4ce67d3e39744b23d9d5f0a2004
leo <leo@pop-os.localdomain>
2024-04-24 22:31:22 -0300

    TP4

--- backend/main/models/autor.py
+++ backend/main/models/autor.py
@@ -1,30 +1,34 @@
 from .. import db
+from .libro import libros_autores
 
 class Autor(db.Model):
+    __tablename__ = "autores"
     idAutor = db.Column(db.Integer, primary_key=True)
     nombre = db.Column(db.String(60), nullable=False)
     apellido = db.Column(db.String(60), nullable=False)
+    apodo = db.Column(db.String(60), nullable=False)
 
     def __repr__(self):
-        return f"<id: {self.idAutor}, Nombre: {self.nombre}, Apellido: {self.apellido}>"
+        return f"<id: {self.idAutor}, Nombre: {self.nombre}, Apellido: {self.apellido}, Apodo: {self.apodo}>"
     
     def to_json(self):
         autor_json = {
             "id" : int(self.idAutor),
-            "Nombre" : str(self.nombre),
-            "Apellido" : str(self.apellido)
+            "nombre" : str(self.nombre),
+            "apellido" : str(self.apellido),
+            "apodo" : str(self.apodo)
         }
+        return autor_json
     
     @staticmethod
     def from_json(autor_json):
         id = autor_json.get("id")
-        nombre = autor_json.get("Nombre")
-        apellido = autor_json.get("Apellido")
+        nombre = autor_json.get("nombre")
+        apellido = autor_json.get("apellido")
+        apodo = autor_json.get("apodo")
         return Autor(
             idAutor=id,
             nombre=nombre,
-            apellido=apellido
+            apellido=apellido,
+            apodo=apodo
         )
-    
-if __name__ == '__main__':
-    pass
