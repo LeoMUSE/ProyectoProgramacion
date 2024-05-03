a46a663c2a36d35f0684e8a3e6dc7a45819bb9be
VicenteCaraT <vit.cara@alumno.um.edu.ar>
2024-05-03 15:06:52 -0300

    updating init(reseña)

--- backend/main/models/__init__.py
+++ backend/main/models/__init__.py
@@ -5,3 +5,4 @@ from .comentario import Comentario as ComentarioModel
 from .autor import Autor as AutorModel
 from .prestamo import Prestamo as PrestamoModel
 from .valoracion import Valoracion as ValoracionModel
+from .reseña import Reseña as ReseñaModel
