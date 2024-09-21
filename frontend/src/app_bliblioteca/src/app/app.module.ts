7c1cc87b19707b44d5976123e321bc609a577984
VicenteCaraT <vit.cara@alumno.um.edu.ar>
2024-09-21 14:19:08 -0300

    adding home and navbar,categories components

--- frontend/src/app_bliblioteca/src/app/app.module.ts
+++ frontend/src/app_bliblioteca/src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './pages/home/home.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { CategoriesComponent } from './components/categories/categories.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    NavbarComponent,
+    CategoriesComponent
   ],
   imports: [
     BrowserModule,
