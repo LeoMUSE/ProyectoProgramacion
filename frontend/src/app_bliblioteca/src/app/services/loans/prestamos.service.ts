cdc99b1f9cdc70a194b7a776eaf698dbb8267417
VicenteCaraT <vit.cara@alumno.um.edu.ar>
2024-10-14 14:28:08 -0300

    fixing services, TODO: fix backend

--- frontend/src/app_bliblioteca/src/app/services/loans/prestamos.service.ts
+++ frontend/src/app_bliblioteca/src/app/services/loans/prestamos.service.ts
@@ -5,7 +5,7 @@ import { Injectable } from '@angular/core';
   providedIn: 'root'
 })
 export class PrestamosService {
-  url = '/Prestamos'
+  url = '/api'
 
   constructor(
     private httpClient: HttpClient,
