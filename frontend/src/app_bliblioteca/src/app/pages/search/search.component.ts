829cf4f0e1a3255d9ab61147a44f140de10d40ab
VicenteCaraT <vit.cara@alumno.um.edu.ar>
2024-10-28 13:41:04 -0300

    debuggin

--- frontend/src/app_bliblioteca/src/app/pages/search/search.component.ts
+++ frontend/src/app_bliblioteca/src/app/pages/search/search.component.ts
@@ -13,21 +13,15 @@ export class SearchComponent {
   constructor(private bookService: LibrosService) {}
 
   handleSearch(query: string) {
-    console.log('Search query:', query); // Log para ver el query
     if (query) {
       this.bookService.getBooks(1, { titulo: query }).subscribe(
-        (response: any) => {
-          console.log('Search response:', response); // Log para ver la respuesta
-          
+        (response: any) => {          
           if (response && response.libros) {
             this.searchResults = response.libros;
           } else {
             this.searchResults = [];
           }
-          
-          console.log('Search results:', this.searchResults); // Log para ver los resultados de búsqueda
           this.showDropdown = this.searchResults.length > 0;
-          console.log('Show dropdown:', this.showDropdown); // Log para ver si showDropdown es true
         },
         (error) => {
           console.error('Error al buscar libros:', error);
