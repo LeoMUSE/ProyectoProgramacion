08fdbb29e4dc918463d35044f756e4ab5c4088be
VicenteCaraT <vit.cara@alumno.um.edu.ar>
2024-10-28 13:37:40 -0300

    page search working with titulo filter

--- frontend/src/app_bliblioteca/src/app/pages/search/search.component.ts
+++ frontend/src/app_bliblioteca/src/app/pages/search/search.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { LibrosService } from '../../services/books/libros.service';
 
 @Component({
   selector: 'app-search',
@@ -6,5 +7,37 @@ import { Component } from '@angular/core';
   styleUrl: './search.component.css'
 })
 export class SearchComponent {
+  showDropdown: boolean = false;
+  searchResults: any[] = [];
 
+  constructor(private bookService: LibrosService) {}
+
+  handleSearch(query: string) {
+    console.log('Search query:', query); // Log para ver el query
+    if (query) {
+      this.bookService.getBooks(1, { titulo: query }).subscribe(
+        (response: any) => {
+          console.log('Search response:', response); // Log para ver la respuesta
+          
+          if (response && response.libros) {
+            this.searchResults = response.libros;
+          } else {
+            this.searchResults = [];
+          }
+          
+          console.log('Search results:', this.searchResults); // Log para ver los resultados de búsqueda
+          this.showDropdown = this.searchResults.length > 0;
+          console.log('Show dropdown:', this.showDropdown); // Log para ver si showDropdown es true
+        },
+        (error) => {
+          console.error('Error al buscar libros:', error);
+          this.searchResults = [];
+          this.showDropdown = false;
+        }
+      );
+    } else {
+      this.showDropdown = false;
+      this.searchResults = [];
+    }
+  }
 }
